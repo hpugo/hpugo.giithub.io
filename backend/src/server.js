229c33a17a26a106ffb25f4921f8548316f87e70
Henry Pugo <henry@Henrys-MacBook-Pro-2.local>
2024-03-16 15:00:19 -0500

    adding to render

--- backend/src/server.js
+++ backend/src/server.js
@@ -5,37 +5,40 @@ import cors from 'cors';
 import shoeRouter from './routers/shoes.router.js';
 import userRouter from './routers/user.router.js';
 import { dbconnect } from './config/database.config.js';
-import { dirname, join } from 'path'; // Import `join` from path module
+import { dirname, join } from 'path';
+
 dotenv.config();
 
-dbconnect();
+dbconnect().then(() => {
+  const __filename = fileURLToPath(import.meta.url);
+  const __dirname = dirname(__filename);
 
-const __filename = fileURLToPath(import.meta.url);
-const __dirname = dirname(__filename);
+  const app = express();
+  app.use(express.json());
 
-const app = express();
-app.use(express.json());
+  app.use(
+    cors({
+      credentials: true,
+      origin: ['http://localhost:3000'],
+    })
+  );
 
-app.use(
-  cors({
-    credentials: true,
-    origin: ['http://localhost:3000'],
-  })
-);
+  app.use('/api/shoes', shoeRouter);
+  app.use('/api/users', userRouter);
 
-app.use('/api/shoes', shoeRouter);
-app.use('/api/users', userRouter);
+  const publicFolder = join(__dirname, 'public');
+  app.use(express.static(publicFolder));
 
-const publicFolder = join(__dirname, 'public'); // Use `join` to join paths
-app.use(express.static(publicFolder));
+  app.get('*', (req, res) => {
+    const indexFilePath = join(publicFolder, 'index.html');
+    res.sendFile(indexFilePath);
+  });
 
-app.get('*', (req, res) => {
-  const indexFilePath = join(publicFolder, 'index.html'); // Use `join` to join paths
-  res.sendFile(indexFilePath);
-});
+  const PORT = process.env.PORT || 5000;
 
-const PORT = process.env.PORT || 5000;
-
-app.listen(PORT, () => {
-  console.log('listening on port' + PORT);
+  app.listen(PORT, () => {
+    console.log('Server is running on port ' + PORT);
+  });
+}).catch((error) => {
+  console.error('Failed to connect to the database:', error);
 });
